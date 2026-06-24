d08e1db4ccae7355dd44e37374775c07370854f5
Chris Werner <chris.werner@lunartelephone.com>
2026-06-23 21:29:29 -0700

    Initial Commit

---
+++ Flight Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flight Control
+//
+//  Created by Chris Werner on 6/23/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flight Control/Flight_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_ControlApp.swift
+//  Flight Control
+//
+//  Created by Chris Werner on 6/23/2026.
+//
+
+import SwiftUI
+
+@main
+struct Flight_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
